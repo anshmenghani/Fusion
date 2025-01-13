b7625aabf8ad1d13180c1021a1c1c558a696f95a
anshmenghani <ansh.menghani@gmail.com>
2025-01-13 09:17:43 -0500

    Update fusion.py

--- src/FUSION/fusion.py
+++ src/FUSION/fusion.py
@@ -3,9 +3,9 @@
 '''The Fusion Stellaar Model is designed to predict and classify
 stellar attributes with higher accuracy than traditional formulas. 
 It takes inputs of a star's Effective Temperature, Luminosity, and 
-Radius and runs it's Deep Neural Network algorithm to predict and 
+Radius, and runs its Deep Neural Network algorithm to predict and 
 classify other attributes of a star such as Mass, Surface Gravity, 
-etc. It is trained on ESA's Gaia mission dataset with over 403 
+etc. It is trained on the ESA's Gaia mission dataset with over 403 
 million features.  
 '''
 
