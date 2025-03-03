d726daaadd9d098dce56ea53d6f1f46ece2c9f7b
anshmenghani <ansh.menghani@gmail.com>
2025-03-03 13:19:42 -0500

    citations

--- src/data/htmlProcessing/dataCleaning.py
+++ src/data/htmlProcessing/dataCleaning.py
@@ -1,4 +1,13 @@
 # Copy data dowload links from the ESA GAIA Mission's Astronomical Survey
+'''
+This work has made use of data from the European Space Agency (ESA) mission 
+Gaia (https://www.cosmos.esa.int/gaia), processed by the Gaia Data Processing 
+and Analysis Consortium (DPAC, https://www.cosmos.esa.int/web/gaia/dpac/consortium). 
+Funding for the DPAC has been provided by national institutions, in particular 
+the institutions participating in the Gaia Multilateral Agreement.
+    • Gaia Collaboration et al. (2016b): The Gaia mission (provides a description of the Gaia mission including spacecraft, instruments, survey and measurement principles, and operations);
+    • Gaia Collaboration et al. (2023j): Gaia DR3: Summary of the contents and survey properties.
+'''
 
 import pyperclip
 
